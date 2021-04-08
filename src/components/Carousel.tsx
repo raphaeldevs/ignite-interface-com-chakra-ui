80f47ef0a2facda1d35bfcce4269bbdc33a5a1dd
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-04-08 12:54:12 -0300

    create carousel section

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,10 @@
+import { Heading } from '@chakra-ui/react'
+
+export function Carousel() {
+  return (
+    <Heading textAlign="center" fontWeight="medium" lineHeight="3.375rem">
+      Vamos nessa?<br/>
+      Então escolha seu continente
+    </Heading>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Divider } from '@chakra-ui/react'
 
 import { Header } from '../components/Header'
 import { Hero } from '../components/Hero'
 import { TravelTypes } from '../components/TravelTypes'
+import { Carousel } from '../components/Carousel'
 
 export default function Home() {
   return (
@@ -10,8 +11,25 @@ export default function Home() {
       <Header />
       <Hero />
 
-      <Flex as="main" maxWidth="1160px" justify="center" mx="auto">
+      <Flex
+        as="main"
+        maxWidth="1160px"
+        justify="center"
+        mx="auto"
+        direction="column"
+      >
         <TravelTypes />
+
+        <Divider
+          width="90px"
+          mt="20"
+          mx="auto"
+          mb="3.25rem"
+          borderWidth="2px"
+          borderColor="dark.text"
+        />
+
+        <Carousel />
       </Flex>
     </>
   )
