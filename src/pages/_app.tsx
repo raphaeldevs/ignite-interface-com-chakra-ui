9d8b74044299dfb81aacef73c1cea26bc75b37f7
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-04-08 17:56:11 -0300

    add swiper global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import { theme } from '../styles/theme'
 
+import 'swiper/swiper-bundle.min.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
