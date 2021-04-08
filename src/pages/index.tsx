7157c5a139e4f3e204dd55c63154c322b828c6df
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-04-08 01:51:44 -0300

    create travel type section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
+import { Flex } from '@chakra-ui/react'
+
 import { Header } from '../components/Header'
 import { Hero } from '../components/Hero'
+import { TravelTypes } from '../components/TravelTypes'
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+
+      <Flex as="main" maxWidth="1160px" justify="center" mx="auto">
+        <TravelTypes />
+      </Flex>
     </>
   )
 }
