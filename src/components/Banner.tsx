657656c517c9c34ea223f947aa2d1f1d0143a059
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-04-07 23:55:22 -0300

    create banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,38 @@
+import { Flex, Box, Heading, Text, Image } from '@chakra-ui/react'
+
+export function Banner() {
+  return (
+    <Flex
+      height="21rem"
+      px="8.75rem"
+      backgroundImage="url('/images/background.png')"
+      backgroundSize="cover"
+    >
+      <Flex
+        maxWidth="1160px"
+        width="100%"
+        mx="auto"
+        justify="space-between"
+        align="center"
+      >
+        <Box color="light.text">
+          <Heading fontWeight="medium" lineHeight="3.375rem">
+            5 Continentes, <br />
+            infinitas possibilidades.
+          </Heading>
+
+          <Text fontSize="1.25rem" mt="1.25rem">
+            Chegou a hora de tirar do papel a viagem que você <br />
+            sempre sonhou.
+          </Text>
+        </Box>
+
+        <Image
+          mb="-32"
+          src="images/airplane.svg"
+          alt="Avião voando entre as nuvens"
+        />
+      </Flex>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { Header } from '../components/Header'
+import { Banner } from '../components/Banner'
 
 export default function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Banner />
+    </>
   )
 }
